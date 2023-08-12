79977157ff2b8ac388f805ff12b9d13d26ec9ca6
Hossein Salari <hosein.salari81@gmail.com>
2023-08-12 17:06:05 +0330

    Filter todos

--- index.js
+++ index.js
@@ -5,26 +5,16 @@ const todos = [];
 const todoInput = document.querySelector(".todo-input");
 const todoForm = document.querySelector(".todo-form");
 const todoList = document.querySelector(".todolist");
+const selectFilter = document.querySelector(".filter-todos");
 
 // events
 
 todoForm.addEventListener("submit", addNewTodo);
+selectFilter.addEventListener("change", filterTodos);
 
 // functions
 
-function addNewTodo(e) {
-  e.preventDefault();
-
-  const newTodo = {
-    id: Date.now(),
-    createdAt: new Date().toISOString(),
-    title: todoInput.value,
-    isCompleted: false,
-  };
-
-  todos.push(newTodo);
-
-  // create todos on DOM
+function createTodos(todos) {
   let result = "";
 
   todos.forEach((todo) => {
@@ -39,5 +29,44 @@ function addNewTodo(e) {
   });
 
   todoList.innerHTML = result;
+}
+
+function addNewTodo(e) {
+  e.preventDefault();
+
+  const newTodo = {
+    id: Date.now(),
+    createdAt: new Date().toISOString(),
+    title: todoInput.value,
+    isCompleted: false,
+  };
+
+  todos.push(newTodo);
+
+  // create todos on DOM
+  createTodos(todos);
   todoInput.value = "";
+}
+
+function filterTodos(e) {
+  const filter = e.target.value;
+
+  switch (filter) {
+    case "all": {
+      createTodos(todos);
+      break;
+    }
+
+    case "completed": {
+      const filteredTodos = todos.filter((todo) => todo.isCompleted);
+      createTodos(filteredTodos);
+      break;
+    }
+
+    case "uncompleted": {
+      const filteredTodos = todos.filter((todo) => !todo.isCompleted);
+      createTodos(filteredTodos);
+      break;
+    }
+  }
 }
